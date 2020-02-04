2ec0e3173aad54d42ae82b8f0ede2cf6ecca40a3
Vitaly Oryschuk <Leilyua@ukr.net>
2020-02-04 12:40:31 +0200

    Updates

---
+++ precache-manifest.15e5012cac7f41884c1ab12cf1c5c6d4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6df7fa9b06d2f957019d50b8b00eef75",
+    "url": "/practic-redux/index.html"
+  },
+  {
+    "revision": "690ca3dabfaa3d4f34ef",
+    "url": "/practic-redux/static/css/main.0f54525f.chunk.css"
+  },
+  {
+    "revision": "2a7c27b64dd463f03f72",
+    "url": "/practic-redux/static/js/2.17abbbc1.chunk.js"
+  },
+  {
+    "revision": "8ae5483c02431091d2b880ebef79f116",
+    "url": "/practic-redux/static/js/2.17abbbc1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "690ca3dabfaa3d4f34ef",
+    "url": "/practic-redux/static/js/main.59945c15.chunk.js"
+  },
+  {
+    "revision": "4360c997f9cf63ed4696",
+    "url": "/practic-redux/static/js/runtime-main.ca404e0d.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "//github.com/VitalyOryschuk/Goit-React.js/precache-manifest.bc108172e57f9e3741c87761ec55bf1c.js"
+  "/practic-redux/precache-manifest.15e5012cac7f41884c1ab12cf1c5c6d4.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("//github.com/VitalyOryschuk/Goit-React.js/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/practic-redux/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
